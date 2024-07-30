9c76bc482282ae328a793c2f072d4c6670530418
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-07-30 23:37:31 +0600

    admin Sign up design add

--- client/src/pages/admin/AdminSignup.jsx
+++ client/src/pages/admin/AdminSignup.jsx
@@ -1,31 +1,22 @@
-import React from 'react'
+
 import Btn from '../../component/Btn'
+import Form from '../../component/Form'
+import FormBody from '../../component/FormBody'
+import Input from '../../component/Input'
 
 export const AdminSignup = () => {
-    const handelSubmit = ()=>{
-
-    }
   return (
-    <secton className="min-h-screen  flex justify-center font-roboto items-center">
-        <div className='p-10 min-w-[40vw] min-h-[70vh] border-2 bg-orange-600 rounded-lg'>
-            <h3 className='text-center mb-4 text-xl font-bold  text-white'>Admin Login</h3>
-            <form onSubmit={handelSubmit} 
-                className='flex flex-col gap-6 '>
-                <input type="text" 
-                className='outline-none py-2 pl-2 text-base rounded-md'
-                placeholder='name'/>
-                <input type="text"  
-                className='outline-none py-2 pl-2 text-base rounded-md'
-                placeholder='username'/>
-                <input type="email" 
-                className='outline-none py-2 pl-2 text-base rounded-md'  placeholder='email'/>
-                <input type="text"  
-                className='outline-none py-2 pl-2 text-base rounded-md' placeholder='secret question'/>
-                <input type="password" 
-                className='outline-none py-2 pl-2 text-base rounded-md'  placeholder='password'/>
-                <Btn innerText="Sign Up" className=""/>
-            </form>
-        </div>
-    </secton>
+    <FormBody>
+        <Form>
+            <Input type="text" placeholder="name"/>
+            <Input type="email" placeholder="email"/>
+            <Input type="text" placeholder="username"/>
+            <Input type="text" placeholder="secret question"/>
+            <Input type="text" placeholder="password"/>
+            <Btn innerText="Sign Up"/>
+        </Form>
+    </FormBody>
   )
 }
+
+
