85f072021a8fdedd1868feafb4807e287a9d8b68
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-07-31 19:53:08 +0600

    student slice add to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import adminReducer from './slices/admin/adminSlice'
-
+import studentReducer from './slices/student/studentSlice'
 export const store = configureStore({
                     
             reducer : {
-                admins : adminReducer
+                admin : adminReducer,
+                student : studentReducer
             }
 })
