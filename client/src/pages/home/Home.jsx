911d9111b308a9b31dcc897629eb0b5612dd154c
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-08-01 23:40:30 +0600

    home page update

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,7 +4,9 @@ import NavItem from '../../component/NavItem'
 import Btn from '../../component/Btn'
 import  Image  from '../../component/Image'
 import imgLink from '../../assets/home.png'
+import { useNavigate } from 'react-router-dom'
 export const Home = () => {
+    const navigate = useNavigate()
   return (
     <section className='max-w-container py-20 mx-auto font-roboto'>
         <NavLink className="justify-between gap-4">
@@ -16,9 +18,9 @@ export const Home = () => {
                 <h1 className="text-[36px]  font-[800] leading-10">Welcome to <br/>school management <br/>system</h1>
                     <p className='text-base'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem harum eligendi quisquam adipisci, voluptas labore fuga alias et magnam beatae! Sit praesentium ex voluptatem iste ducimus et ipsa odit qui.</p>
                     <NavLink className="flex-col gap-4">
-                        <Btn innerText="login"/>
-                        <Btn innerText="login as guest" className="bg-teal-700 text-white hover:border-cyan-90 hover:text-teal-700 mb-10"/>
-                    <p className='text-center text-base'>Don't have an account? <NavItem innerContent="sign up"/></p>
+                        <Btn onClick={()=>navigate("/select-roll")} innerText="login"/>
+                        <Btn onClick={()=>navigate("/select-roll")}  innerText="login as guest" className="bg-teal-700 text-white hover:border-cyan-90 hover:text-teal-700 mb-10"/>
+                    <p onClick={()=>navigate("/select-roll")}  className='text-center text-base cursor-pointer'>Don't have an account? <NavItem innerContent="sign up"/></p>
                     </NavLink>
                 </NavLink>
             </div>
